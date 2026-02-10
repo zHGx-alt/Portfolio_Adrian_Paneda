04791f13028ce0408ee7e7ae854377764491aa89
zHGx-alt <not.thoradri@gmail.com>
2026-02-10 12:08:09 +0100

    updt after a while

--- ecommerce-analytics-project/scripts/03_load_sqlite.py
+++ ecommerce-analytics-project/scripts/03_load_sqlite.py
@@ -2,21 +2,16 @@ import pandas as pd
 import sqlite3
 import os
 
-# Ruta al CSV
 csv_path = "/home/zhgx/Portfolio_Adrian_Paneda/ecommerce-analytics-project/data_sample/clean_data.csv"
 
-# Comprobación básica
 if not os.path.exists(csv_path):
     raise FileNotFoundError(f"No existe el archivo: {csv_path}")
 
-# Leer datos
 df = pd.read_csv(csv_path)
 
-# Crear / conectar a la base de datos
 db_path = "/home/zhgx/Portfolio_Adrian_Paneda/ecommerce-analytics-project/sql/ecommerce.db"
 conn = sqlite3.connect(db_path)
 
-# Volcar a SQLite
 df.to_sql(
     name="clean_data",
     con=conn,
